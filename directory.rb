c71d94df235bb66f3efc07fcc49c8c7534be076d
Josh Bebbington <josh@focalwebsites.com>
2014-12-02 13:25:40 +0000

    Added basic count of students

---
+++ directory.rb
@@ -0,0 +1,15 @@
+puts "The students of my cohort at Makers Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex De Large"
+puts "The Alien"
+puts "Terminator"
+puts "Freddy Kruger"
+puts "The Joker"
+
+print "Overall, we have "
+print 9
+print " students!"
