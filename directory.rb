b2f7d648493fa66b51428a905449bc7d65922aec
Josh Bebbington <josh@focalwebsites.com>
2014-12-02 13:31:51 +0000

    Refactored student list to an array, and the student count to the length of that array

--- directory.rb
+++ directory.rb
@@ -1,15 +1,25 @@
-student_count = 9
+students = [
+	"Dr. Hannibal Lecter",
+	"Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex De Large",
+    "The Alien",
+    "Terminator",
+    "Freddy Kruger",
+    "The Joker",
+	]
 
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex De Large"
-puts "The Alien"
-puts "Terminator"
-puts "Freddy Kruger"
-puts "The Joker"
+puts "The students at Makers Academy: "
+puts "---------"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
 
-puts "Overall we have #{student_count} students!"
+puts "Overall we have #{students.length} students!"
