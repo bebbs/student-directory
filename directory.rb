6477f500b3fc83e703246335c98800aa78a323e1
Josh Bebbington <josh@focalwebsites.com>
2014-12-02 13:27:23 +0000

    Refactored student_count to a variable

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 9
+
 puts "The students of my cohort at Makers Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -10,6 +12,4 @@ puts "Terminator"
 puts "Freddy Kruger"
 puts "The Joker"
 
-print "Overall, we have "
-print 9
-print " students!"
+puts "Overall we have #{student_count} students!"
